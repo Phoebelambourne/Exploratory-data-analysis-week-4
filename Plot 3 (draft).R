5bac72fd85479c174231619b053d204d6129e235
phoebelambourne <zcbtpkl@ucl.ac.uk>
2022-05-29 10:12:13 +0100

    Updated script - stuck at ggplot

--- Plot 3 (draft).R
+++ Plot 3 (draft).R
@@ -24,10 +24,12 @@ nonroad <- NEI %>%
   group_by(year) %>%
   summarise(sum(Emissions))
 
-rename(point, Emissions = `sum(Emissions)`) 
-rename(nonpoint, Emissions = `sum(Emissions)`)
-rename(onroad, Emissions = `sum(Emissions)`)
-rename(nonroad, Emisions = `sum(Emissions)`)
+point <- rename(point, Emissions.P = `sum(Emissions)`) 
+nonpoint <- rename(nonpoint, Emissions.NP  = `sum(Emissions)`)
+onroad <- rename(onroad, Emissions.NR   = `sum(Emissions)`)
+nonroad <- rename(nonroad, Emissions.ON  = `sum(Emissions)`)
+
+
 
 # Can I do the above in one command?
 
@@ -39,6 +41,23 @@ plot(onroad, type = "l", ylab = "Emissions", main = "On-Road")
 plot(nonroad, type = "l", ylab = "Emissions", main = "Non-Road")
 
 # Plot - ggplot 
+
+joinedp <- left_join(point, nonpoint, by = 'year')
+joinedr <- left_join(onroad, nonroad, by = 'year')
+merged <- left_join(joinedp, joinedr, by = 'year')
+head(merged)
+
+longer_data <- merged %>% 
+  pivot_longer(Emissions.P:Emissions.ON, names_to = "type", values_to = "Emissions")
+
+longer_data
+library(ggplot2)
+
+# Stuck here at how to plot year by emissions, for each type
+
+
+
+
 
 
 
