8bed01604c2b62047fd8118e453039d59ae7691b
phoebelambourne <zcbtpkl@ucl.ac.uk>
2022-05-29 11:58:57 +0100

    corrected plot names

--- Plot 5 script .R
+++ Plot 5 script .R
@@ -3,6 +3,9 @@
 # How have emissions from motor vehicle sources changed 
 # from 1999–2008 in Baltimore City?
 
+library(dplyr)
+library(ggplot2)
+
 vehiclesbm <- subset(NEI, fips == '24510' & type == 'ON-ROAD')
 SCConroad <- subset(SCC, Data.Category == 'Onroad')
 
@@ -14,23 +17,23 @@ sums <- merged %>%
    rename(Emissions = 'sum(Emissions)') %>% 
    rename(SCC = 'SCC.Level.Three')
 
-head(sums)
-
-ggplot(sums, aes(x = year, y = Emissions, col = SCC)) 
-+ geom_line() + ggtitle(expression("Emissions from Motor Vehicle Sources in Baltimore City"))
+ggplot(sums, aes(x = year, y = Emissions, col = SCC)) +
+  geom_line() + labs(title = "Emissions from Motor Vehicle Sources in Baltimore City")
+  
+dev.copy(png, "Plot 5a.png")
+dev.off()
 
 totalsums <- merged %>% 
   group_by(year) %>% 
   summarise(sum(Emissions)) %>% 
   rename(Emissions = 'sum(Emissions)')
 
-dev.copy(png, "Plot 4a.png")
+dev.copy(png, "Plot 5a.png")
 dev.off()
 
 ggplot(totalsums, aes(x = year, y = Emissions)) +
-  geom_line() + ggtitle(expression(" Total Emissions 
-                        from Motor Vehicle Sources in Baltimore City"))
+  geom_line() + labs(title = "Total Emissions from Motor Vehicle Sources in Baltimore City")
 
-dev.copy(png, "Plot 4b.png")  
+dev.copy(png, "Plot 5b.png")  
 dev.off()  
 
